a4906aa7ab31da25dfeb726821da0c7f359bff71
atish <atish@DESKTOP-LLBK2PA>
2020-10-21 07:07:38 +0600

    images added

--- loop/while_digit_sum.c
+++ loop/while_digit_sum.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(){
-    int number, sum = 0;
-    printf("Enter a number: ");
-    scanf("%d", &number);
-    while(number !=0){
-        sum = sum + number%10;
-        number = number/10;
-    }
-    printf("Digit sum of is: %d", sum);
+int number, sum = 0;
+printf("Enter a number: ");
+scanf("%d", &number);
+while(number !=0){
+    sum = sum + number%10;
+    number = number/10;
+}
+printf("Digit sum of is: %d", sum);
     return 0;
 }
+
